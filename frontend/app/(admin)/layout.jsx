43cf6da5abf79a6beaf9b310095f47f066e4fcf9
Bijan Shakya <bijanshakya145@gmail.com>
2026-03-21 19:21:25 +0545

    project frontend and backend initiated

---
+++ frontend/app/(admin)/layout.jsx
@@ -0,0 +1,8 @@
+export default function layout({ children }) {
+  return (
+    <div>
+      this is the thap page
+      {children}
+    </div>
+  );
+}
---
+++ frontend/app/(public)/layout.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function layout({ children }) {
+  return <div>this is the public layout{children}</div>;
+}
